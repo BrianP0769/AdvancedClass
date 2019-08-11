22b0f795275403186e91dfff938a43e513b8915f
BrianP0769 <47798398+BrianP0769@users.noreply.github.com>
2019-08-10 20:58:25 -0400

    Add files via upload

--- Hw.java
+++ Hw.java
@@ -1,61 +1,46 @@
 import java.util.*;
-public class Hw{
+public class Lesson{
 
-        public static int checkDist(Integer start, Integer finish){
-                int dist = 0;
-                Queue L1 = new Queue();
-                Queue temp = new Queue();
-                L1.enqueue(start);
-                while(true){
-                        while(L1.isEmpty() == false){
-                                if(L1.peek() != finish){
-                                        temp.enqueue(hmap.get(L1.peek()));
-                                        System.out.println(L1.peek());
-                                        L1.dequeue();
-                                }
-                                else{
-                                        return dist;
-                                }
-                        }
-                        L1 = temp;
-                        temp = new Queue();
-                        dist ++;
-                }
+
+        public static void main(String[] args) {
+                Node start = new Node();
+                Node A  = new Node();
+                Node B = new Node();
+                Node finish = new Node();
+                start.children.add(A);
+                start.lengths.add(6);
+                start.children.add(B);
+                start.lengths.add(2);
+                A.children.add(finish);
+                A.lengths.add(1);
+                B.children.add(A);
+                B.lengths.add(3);
+                B.children.add(finish);
+                B.lengths.add(5);
+
+                n = checkDist(start, finish);
+                System.out.println(n);
         }
 
-        static HashMap<Integer, Integer> hmap = new HashMap<>();
-        public static void main(String[] args) {
-                hmap.put(0, 1);
-                hmap.put(0, 2);
-                hmap.put(1, 4);
-                hmap.put(2, 3);
-                hmap.put(2, 5);
-                hmap.put(3, 4);
-                hmap.put(4, 6);
-                hmap.put(5, 4);
-                hmap.put(6, 7);
-                System.out.println("distance:" + checkDist(0,7));
+        public static int checkDist(Node start, Node finish){
+                HashMap<Node, Integer> costs = new HashMap<>();
+                HashMap<Node, Node> parents = new HashMap<>();
+                for(int i = 0; i < start.children.size(); i ++){
+                        costs.put(start.children.get(i), start.lengths.get(i));
+                        parents.put(start.children.get(i), start);
+                }
+                while(true){
+                        for()
+                }
         }
 }
 
-class Queue{
-        private ArrayList<Integer> data;
-        public Queue(){
-                data = new ArrayList<>();
+class Node{
+        ArrayList<Node> children;
+        ArrayList<Integer> lengths;
+        public Node(){
+                children = new ArrayList<>();
+                lengths= new ArrayList<>();
         }
-        public void enqueue(Integer x){
-                data.add(x);
-        }
-        public void dequeue(){
-                data.remove(0);
-        }
-        public int length(){
-                return data.size();
-        }
-        public boolean isEmpty(){
-                return data.isEmpty();
-        }
-        public Integer peek(){
-                return data.get(0);
-        }
+
 }
